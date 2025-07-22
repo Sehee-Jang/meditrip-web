15ea73d5928643ff29a7765fd8c263d17ee73038
Sehee Jang <seheejang94@gmail.com>
2025-07-22 16:43:41 +0900

    del: 사용하지 않는 코드 삭제

--- src/app/[locale]/contents/page.tsx
+++ src/app/[locale]/contents/page.tsx
@@ -1,7 +1,7 @@
 import { getTranslations } from "next-intl/server";
 import SearchableContents from "@/components/SearchableContents";
 import Container from "@/components/layout/Container";
-import { ChevronLeft } from "lucide-react";
+
 import PageHeader from "@/components/layout/PageHeader";
 import SignupSection from "@/components/SignupSection";
 
@@ -16,18 +16,8 @@ export default async function ContentsPage() {
         showBackIcon
         center
       />
-      {/* 모바일 헤더 */}
-      {/* <h1 className='flex md:hidden w-full shadow-md text-xl font-bold items-center gap-2 py-3 px-4 mb-4'>
-        <ChevronLeft size={24} />
-        {t("title")}
-      </h1> */}
 
       <Container>
-        {/* 데스크탑 헤더 */}
-        {/* <h1 className='hidden md:block md:text-4xl font-bold text-center my-16 mx-40 '>
-          {t("title")}
-        </h1> */}
-
         <SearchableContents />
         <SignupSection />
       </Container>
