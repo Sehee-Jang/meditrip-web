26b500702ae612f70623a3e6525c2b4bbdbe6f31
Sehee Jang <seheejang94@gmail.com>
2025-09-15 17:37:07 +0900

    fix(route): ArticleDetailPage에서 params Promise를 await하도록 수정

--- src/app/[locale]/(user)/articles/[id]/page.tsx
+++ src/app/[locale]/(user)/articles/[id]/page.tsx
@@ -1,15 +1,21 @@
 import Container from "@/components/common/Container";
 import ArticleDetailClient from "@/components/articles/ArticleDetailClient";
 
+type Params = {
+  id: string;
+  locale: string;
+};
+
 export default async function ArticleDetailPage({
   params,
 }: {
-  params: { id: string; locale: string };
+  params: Promise<Params>;
 }) {
+  const { id } = await params;
   return (
     <main className='md:px-4 md:py-8'>
       <Container>
-        <ArticleDetailClient id={params.id} />
+        <ArticleDetailClient id={id} />
       </Container>
     </main>
   );
